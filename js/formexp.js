04bab1feec61e6b0792e2fbebd9417f091392e94
VukasinLegetic <77751476+VukasinLegetic@users.noreply.github.com>
2021-03-31 16:12:28 +0200

    Add files via upload

--- js/formexp.js
+++ js/formexp.js
@@ -3,7 +3,7 @@ document.getElementById("button").addEventListener("click", function(){
 let name=document.getElementById("Name");
 let email=document.getElementById("Email")
 
-let patName=/^[A-ZČĆŽŠĐ][a-zčćžšđ]{2,20}(\s[A-ZČĆŽŠĐ][a-zčćžšđ]{2,20})*/
+let patName=/^([a-zA-Z]{2,15}\s[a-zA-Z]{1,15}'?-?[a-zA-Z]{2,15}\s?([a-zA-Z]{1,15})?)/
 let patEmail=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 let tName=patName.test(name.value);
